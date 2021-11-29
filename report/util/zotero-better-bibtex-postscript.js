603f55f3d33af7efa6d51cc1a628f3e6e296250a
wonrax <hahuylonghai2012@gmail.com>
2021-11-29 15:29:16 +0700

    + last accessed bibtex export

--- report/util/zotero-better-bibtex-postscript.js
+++ report/util/zotero-better-bibtex-postscript.js
@@ -2,7 +2,12 @@ if (Translator.BetterBibTeX) {
     if (reference.has.howpublished) {
         reference.remove("howpublished");
     }
+
     if (item.url) {
       reference.add({ name: 'url', value: item.url });
     }
+
+    if (item.accessDate) {
+        reference.add({ name: 'note', value: "Truy cập lần cuối " + item.accessDate.replace(/\s*T?\d+:\d+:\d+.*/, '') });
+	}
 }
