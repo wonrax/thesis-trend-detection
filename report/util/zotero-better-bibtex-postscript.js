a0ab63378a3448b39ad80b69f7b1ff06615f9a6f
wonrax <hahuylonghai2012@gmail.com>
2021-11-29 20:23:21 +0700

    add url by zotero preferences instead

--- report/util/zotero-better-bibtex-postscript.js
+++ report/util/zotero-better-bibtex-postscript.js
@@ -3,11 +3,7 @@ if (Translator.BetterBibTeX) {
         reference.remove("howpublished");
     }
 
-    if (item.url) {
-      reference.add({ name: 'url', value: item.url });
-    }
-
-    if (item.accessDate) {
+    if (item.accessDate && ["misc", "webpage"].includes(item.itemType)) {
         reference.add({ name: 'note', value: "Truy cập lần cuối " + item.accessDate.replace(/\s*T?\d+:\d+:\d+.*/, '') });
-	}
+    }
 }
